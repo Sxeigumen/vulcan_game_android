2ac7fef97f4a052812856ac14adbd0a983a6d4bb
VonenZinainn <cer.jarov@gmail.com>
2023-07-26 23:56:14 +0300

    Изменены Toast, чтобы отображать несколько полученных элементов

--- app/src/main/java/com/example/game/Fragment_list.kt
+++ app/src/main/java/com/example/game/Fragment_list.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -32,7 +33,7 @@ class Fragment_list : Fragment(), ElementAdapter.Listener {
                 ELECTROLYZE = true
             }
             elementAdapter.add(it)
-            MAIN.customToast("Получен элемент ${it.NameId}")
+            Toast.makeText(MAIN, "Получен элемент ${it.NameId}", Toast.LENGTH_SHORT).show()
         }
 
         return binding.root
