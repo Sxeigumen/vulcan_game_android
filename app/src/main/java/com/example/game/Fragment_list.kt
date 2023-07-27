d955766631658aad127a1e84a65a1d021493f3e9
VonenZinainn <cer.jarov@gmail.com>
2023-07-27 15:11:03 +0300

    Удалены Toast о получении нового элемента

--- app/src/main/java/com/example/game/Fragment_list.kt
+++ app/src/main/java/com/example/game/Fragment_list.kt
@@ -5,7 +5,6 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -33,7 +32,6 @@ class Fragment_list : Fragment(), ElementAdapter.Listener {
                 ELECTROLYZE = true
             }
             elementAdapter.add(it)
-            Toast.makeText(MAIN, "Получен элемент ${it.NameId}", Toast.LENGTH_SHORT).show()
         }
 
         return binding.root
